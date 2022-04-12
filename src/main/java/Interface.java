034f83f3c1a3b739a0d059f229622df9a3b50c46
Andre Onuki <otanuki@gmail.com>
2022-04-12 14:05:42 -0400

    base project

---
+++ src/main/java/Interface.java
@@ -0,0 +1,13 @@
+/*
+ *
+ *  * Copyright 2021 New Relic Corporation. All rights reserved.
+ *  * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+/**
+ * Description of this interface as a Javadoc
+ */
+public interface Interface {
+
+}
